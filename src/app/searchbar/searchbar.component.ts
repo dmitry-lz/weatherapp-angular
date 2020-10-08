e50e07f75e869d03c58b77728d7b86e3fc4e7fcc
Dmitrii Kupriunin <kupriunin@gmail.com>
2020-10-08 15:13:48 +0300

    Setted up multiselect
    Added

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
+import {
+  switchMap,
+  startWith,
+  withLatestFrom,
+  distinctUntilChanged,
+  debounceTime,
+  filter,
+  map
+} from 'rxjs/operators';
 
 import { API_KEYS } from '../../shared/api-keys';
+
 
 @Component({
   selector: 'app-searchbar',
@@ -11,8 +21,20 @@ import { API_KEYS } from '../../shared/api-keys';
 })
 export class SearchbarComponent implements OnInit {
   myControl = new FormControl(); //dont really know how to name it
-  options: string[] = ['One', 'Two', 'Three'];
-  data;
+  // options: string[] = ['One', 'Two', 'Three'];
+  options;
+  public data = this.myControl.valueChanges.pipe(
+    startWith(this.myControl.value),
+    filter((value) => value?.length >= 3),
+    distinctUntilChanged(),
+    debounceTime(200),
+    switchMap((value) =>
+      this.http.get(
+        `${API_KEYS.geoUrl}?geocode=${value}&apikey=${API_KEYS.geoKey}&format=json`
+      )
+    ),
+    map(data => data.response.GeoObjectCollection.featureMember.map(item => item.GeoObject))
+  );
 
   constructor(
     private http: HttpClient
@@ -21,16 +43,10 @@ export class SearchbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onKeyUp() {
-    if (this.myControl.value.length >= 3) {
-      const URL: string = `${API_KEYS.geoUrl}?geocode=${this.myControl.value}&apikey=${API_KEYS.geoKey}`;
-      this.http.get(URL).subscribe(data => this.data = data); //КАК ЭТО РАБОТАЕТ? 
-      console.log(this.data);
-
-
-
-    }
-
+  onClick() {
+    console.log(this.data);
+    
   }
+  
 
 }
