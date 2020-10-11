f350386b904770ad6de3dc4a17eb92c7d9140b82
Dmitrii Kupriunin <kupriunin@gmail.com>
2020-10-11 15:49:05 +0300

    Searchbar: fixed output data
    Now when option from a multiselect is selected, original object with data
    for a request to openweather api is available

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -13,6 +13,8 @@ import {
 
 import { API_KEYS } from '../../shared/api-keys';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -21,32 +23,39 @@ import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
   styleUrls: ['./searchbar.component.scss']
 })
 export class SearchbarComponent implements OnInit {
-  // @Output()optionSelected: EventEmitter<MatAutocompleteSelectedEvent>;
-
   myControl = new FormControl(); 
-//i dont really know what happens from line 25 to line 34
-  public data = this.myControl.valueChanges.pipe(
-    startWith(this.myControl.value),
-    filter((value) => value?.length >= 3),
-    distinctUntilChanged(),
-    debounceTime(200),
-    switchMap((value) =>
-      this.http.get(
-        `${API_KEYS.geoUrl}?geocode=${value}&apikey=${API_KEYS.geoKey}&format=json`
-      )
-    ),
-    map( (data: any ) => data.response.GeoObjectCollection.featureMember.map(item => item.GeoObject))
-  );
+  displayWith = (value) => value?.name;
+  options = [];
+  optionSubscription: Subscription;
 
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit(): void {
+    const options$ = this.myControl.valueChanges.pipe(
+      startWith(this.myControl.value),
+      filter((value) => value?.length >= 3),
+      distinctUntilChanged(),
+      debounceTime(200),
+      // move to service
+      switchMap((value) =>
+        this.http.get(
+          `${API_KEYS.geoUrl}?geocode=${value}&apikey=${API_KEYS.geoKey}&format=json`
+        )
+      ),
+      map( (data: any ) => data.response.GeoObjectCollection.featureMember.map(item => item.GeoObject))
+    );
+
+    this.optionSubscription = options$.subscribe( options => this.options = options );
+  }
+
+  ngOnDestroy(): void {
+    this.optionSubscription.unsubscribe();
   }
 
   selectValue(event) {
-    console.log(event.option._mostRecentViewValue);
+    console.log(event.option);
     
   }
   
