2b1812c09d06b67e18acbdaed62ee20e2ee269b9
Dmitry Luzanov <luzanov.dm@gmail.com>
2020-10-12 11:48:20 +0300

    Убрал ручную подписку на options
    Можно не подписываться на options вручную, а просто
    использовать пайп | async, тогда мы можем избавиться от
    optionSubscription, ngOnDestroy, и т.д.
    В шаблоне у нас просто будет`*ngFor="let option of options | async"`.

--- src/app/weather/searchbar/searchbar.component.ts
+++ src/app/weather/searchbar/searchbar.component.ts
@@ -1,37 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
+
 import {
   switchMap,
   startWith,
   distinctUntilChanged,
   debounceTime,
   filter,
-  map
+  map,
 } from 'rxjs/operators';
 
-import { API_KEYS } from '../../shared/api-keys';
-import { Subscription } from 'rxjs';
-
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.scss']
+  styleUrls: ['./searchbar.component.scss'],
 })
 export class SearchbarComponent implements OnInit {
-  myControl = new FormControl(); 
-  displayWith = (value) => value?.name;
-  options = [];
-  selectedPlace: object;
-  optionSubscription: Subscription;
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  myControl = new FormControl();
+  options: Observable<any[]>;
+  selectedPlace: object;
+  displayWith = (value) => value?.name;
 
   ngOnInit(): void {
-    const options$ = this.myControl.valueChanges.pipe(
+    this.options = this.myControl.valueChanges.pipe(
       startWith(this.myControl.value),
       filter((value) => value?.length >= 3),
       distinctUntilChanged(),
@@ -39,22 +36,18 @@ export class SearchbarComponent implements OnInit {
       // move to service
       switchMap((value) =>
         this.http.get(
-          `${API_KEYS.geoUrl}?geocode=${value}&apikey=${API_KEYS.geoKey}&format=json`
+          `${environment.api_keys.geoUrl}?geocode=${value}&apikey=${environment.api_keys.geoKey}&format=json`
         )
       ),
-      map( (data: any ) => data.response.GeoObjectCollection.featureMember.map(item => item.GeoObject))
+      map((data: any) =>
+        data.response.GeoObjectCollection.featureMember.map(
+          (item) => item.GeoObject
+        )
+      )
     );
-
-    this.optionSubscription = options$.subscribe( options => this.options = options );
-  }
-
-  ngOnDestroy(): void {
-    this.optionSubscription.unsubscribe();
   }
 
   selectValue(event) {
     this.selectedPlace = event.option.value;
   }
-  
-
 }
